08c6aff76642f36d3ede56131962cbef12f8f479
dpyzo0o <yang.xu940121@outlook.com>
2019-09-17 10:37:15 +0800

    initial commit

---
+++ client/src/components/menu-item.js
@@ -0,0 +1,27 @@
+import styled from '@emotion/styled'
+import { css } from '@emotion/core'
+import { Link } from '@reach/router'
+import { colors, unit } from '../styles'
+
+export const menuItemClassName = css({
+  flexGrow: 1,
+  width: 0,
+  fontFamily: 'inherit',
+  fontSize: 20,
+  color: 'inherit',
+  letterSpacing: 1.5,
+  textTransform: 'uppercase',
+  textAlign: 'center',
+  svg: {
+    display: 'block',
+    width: 60,
+    margin: `0 auto ${unit}px`,
+    fill: colors.secondary,
+  },
+})
+
+const MenuItem = styled(Link)(menuItemClassName, {
+  textDecoration: 'none',
+})
+
+export default MenuItem
