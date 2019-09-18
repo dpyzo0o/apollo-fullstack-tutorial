475dd39d83c94f321e76d3847e848b9f60ec8b88
dpyzo0o <yang.xu940121@outlook.com>
2019-09-18 16:58:35 +0800

    chore: tweaks

--- client/src/containers/action-button.js
+++ client/src/containers/action-button.js
@@ -45,6 +45,23 @@ export default function ActionButton({ isBooked, id, isInCart }) {
         variables: { launchId: id },
       },
     ],
+    update: cache => {
+      const { launch } = cache.readQuery({
+        query: IS_LAUNCH_IN_CART,
+        variables: { launchId: id },
+      })
+
+      cache.writeQuery({
+        query: IS_LAUNCH_IN_CART,
+        variables: { launchId: id },
+        data: {
+          launch: {
+            ...launch,
+            isInCart: !launch.isInCart,
+          },
+        },
+      })
+    },
   })
 
   const [toggleCart] = useMutation(TOGGLE_CART, {
