f846efc5e7c67e29689a9a77f300ca7ff9d6799f
dpyzo0o <yang.xu940121@outlook.com>
2019-10-11 14:24:41 +0800

    test: fix test for pages/cart

--- client/src/pages/__tests__/cart.js
+++ client/src/pages/__tests__/cart.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 
 import { renderApollo, cleanup, waitForElement } from '../../test-utils'
 import Cart, { GET_CART_ITEMS } from '../cart'
@@ -8,24 +9,44 @@ describe('Cart Page', () => {
   afterEach(cleanup)
 
   it('renders with message for empty carts', () => {
+    const cache = new InMemoryCache()
+    cache.writeQuery({
+      query: GET_CART_ITEMS,
+      data: { cartItems: [] },
+    })
+
     let mocks = [
       {
         request: { query: GET_CART_ITEMS },
         result: { data: { cartItems: [] } },
       },
     ]
-    const { getByTestId } = renderApollo(<Cart />, { mocks })
+    const { getByTestId } = renderApollo(<Cart />, {
+      mocks,
+      cache,
+      resolvers: {},
+    })
     return waitForElement(() => getByTestId('empty-message'))
   })
 
   it('renders cart', () => {
+    const cache = new InMemoryCache()
+    cache.writeQuery({
+      query: GET_CART_ITEMS,
+      data: { cartItems: [1] },
+    })
+
     let mocks = [
       {
         request: { query: GET_CART_ITEMS },
         result: { data: { cartItems: [1] } },
       },
     ]
-    const { getByTestId } = renderApollo(<Cart />, { mocks })
+    const { getByTestId } = renderApollo(<Cart />, {
+      mocks,
+      cache,
+      resolvers: {},
+    })
     return waitForElement(() => getByTestId('book-button'))
   })
 })
